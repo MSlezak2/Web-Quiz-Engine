ac1f255b33337880f535a542044ec028f66959e9
MSlezak2 <michalsleezak@gmail.com>
2020-12-30 11:13:06 +0100

    Add validation of posted data
    Using hibernate validator, some restrictions
    on posted data (represented by Question
    class) has been put.

--- src/main/java/mslezak2/web_quiz_engine/Controller.java
+++ src/main/java/mslezak2/web_quiz_engine/Controller.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /** Controller that handles clients requests such as fetching the question and submitting the answer */
 @RestController
@@ -12,7 +14,7 @@ public class Controller {
 
     ArrayList<Question> questions = new ArrayList<>(); // it's going to be replaced with the database in the future
     {
-        questions.add(new Question("q1", "t1", new String[]{"a1","a2","a3","a4"}, 2)); // initializing list for testing purposes
+        questions.add(new Question("q1", "t1", new String[]{"a1","a2","a3","a4"}, new int[]{2,3})); // initializing list for testing purposes
     }
 
     @GetMapping("/api/quizzes/{id}")
@@ -33,10 +35,11 @@ public class Controller {
 
     /**POST request should contain single int value representing chosen answer inside its body*/
     @PostMapping("/api/quizzes/{id}/solve")
-    private AnswerFeedback postAnswer(@PathVariable int id, @RequestBody int answer) {
+    private AnswerFeedback postAnswer(@PathVariable int id, @RequestBody int[] answer) {
         if (id < questions.size()) {
         
-            if (answer == questions.get(id).getAnswer()) {
+//            if (Arrays.equals(answer,questions.get(id).getAnswer()) ) {
+            if (Arrays.equals(answer,questions.get(id).getAnswer())) {
                 return AnswerFeedback.POSITIVE_FEEDBACK;
             } else {
                 return AnswerFeedback.NEGATIVE_FEEDBACK;
@@ -51,7 +54,7 @@ public class Controller {
     
     /**Method lets upload new questions defined by the user*/
     @PostMapping("/api/quizzes")
-    private Question createQuestion(@RequestBody Question question) {
+    private Question createQuestion(@Valid @RequestBody Question question) {
         //TODO: Is there any more elegant way to handle that id setting?
         question.setId(questions.size()); //every question gets its unique id
         questions.add(question);
--- src/main/java/mslezak2/web_quiz_engine/Question.java
+++ src/main/java/mslezak2/web_quiz_engine/Question.java
@@ -1,26 +1,30 @@
 package mslezak2.web_quiz_engine;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 /** Class represents single question as a part of the quiz. It contains of:<br>
  *  - unique id<br>
  * - title<br>
  * - text (the actual question)<br>
  * - array of options (the 3rd one is always the correct one)*/
+
 public class Question {
     
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private int id;
-    
+    @NotBlank
     private String title;
+    @NotBlank
     private String text;
+    @Size(min=2)
     private String[] options;
-    
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private int answer;
+    private int[] answer;
     
-    public Question(String title, String text, String[] options, int answer) {
+    public Question(String title, String text, String[] options, int[] answer) {
         this.title = title;
         this.text = text;
         this.options = options;
@@ -36,12 +40,12 @@ public class Question {
     }
     
 //    @JsonProperty
-    public void setAnswer(int answer) {
+    public void setAnswer(int[] answer) {
         this.answer = answer;
     }
     
 //    @JsonIgnore
-    public int getAnswer() {
+    public int[] getAnswer() {
         return answer;
     }
     
