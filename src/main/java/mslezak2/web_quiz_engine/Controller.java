8efe2f86c525b3cb8d9b4ccd146dcbeb557083fc
MSlezak2 <michalsleezak@gmail.com>
2021-01-02 23:58:24 +0100

    Refactor and clean the code
    PostAnswer method has been changed. From now
    on, it requires JSON file as a body of a POST
    request. Also general cleaning of the code
    has been done.

--- src/main/java/mslezak2/web_quiz_engine/Controller.java
+++ src/main/java/mslezak2/web_quiz_engine/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /** Controller that handles clients requests such as fetching the question and submitting the answer */
 @RestController
@@ -19,6 +20,7 @@ public class Controller {
 
     @GetMapping("/api/quizzes/{id}")
     private Question getQuestion(@PathVariable int id) {
+        
         if (id < questions.size()) {
             return questions.get(id);
         } else {
@@ -30,16 +32,24 @@ public class Controller {
     
     @GetMapping("/api/quizzes")
     private ArrayList<Question> getAllQuestions() {
-        return questions;
-    }
-
-    /**POST request should contain single int value representing chosen answer inside its body*/
-    @PostMapping("/api/quizzes/{id}/solve")
-    private AnswerFeedback postAnswer(@PathVariable int id, @RequestBody int[] answer) {
-        if (id < questions.size()) {
         
-//            if (Arrays.equals(answer,questions.get(id).getAnswer()) ) {
-            if (Arrays.equals(answer,questions.get(id).getAnswer())) {
+        return questions;
+        
+    }
+    
+    /**
+     * POST request should contain JSON object inside its body with single property named "answer" with int
+     * as a value representing chosen answer
+     */
+    @PostMapping("/api/quizzes/{id}/solve")
+    private AnswerFeedback postAnswer(@PathVariable int id, @RequestBody HashMap<String, int[]> answer) {
+        if (id < questions.size()) {
+            // TODO: Should answer parameter be of that type? Is there any other, cooler solution?
+            
+            int[] postedAnswer = answer.get("answer");
+            int[] correctAnswer = questions.get(id).getAnswer();
+        
+            if (isAnswerCorrect(postedAnswer, correctAnswer)) {
                 return AnswerFeedback.POSITIVE_FEEDBACK;
             } else {
                 return AnswerFeedback.NEGATIVE_FEEDBACK;
@@ -50,14 +60,32 @@ public class Controller {
                     HttpStatus.NOT_FOUND, "(not found)"
             );
         }
+    
     }
     
     /**Method lets upload new questions defined by the user*/
     @PostMapping("/api/quizzes")
     private Question createQuestion(@Valid @RequestBody Question question) {
+        
         //TODO: Is there any more elegant way to handle that id setting?
         question.setId(questions.size()); //every question gets its unique id
         questions.add(question);
         return questions.get(questions.size() - 1);
+        
+    }
+    
+    
+    private boolean isAnswerCorrect(int[] postedAnswer, int[] correctAnswer) {
+        
+        boolean correct = false;
+        if (Arrays.equals(postedAnswer, correctAnswer)
+            || (postedAnswer == null && correctAnswer.length == 0)
+            || (postedAnswer.length == 0 && correctAnswer == null) ) {
+            
+            correct = true;
+            
+        }
+        return correct;
+        
     }
 }
