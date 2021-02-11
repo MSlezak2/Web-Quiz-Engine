d61e068ec936e3f6bc7d6268fe0b8806e7e486bf
MSlezak2 <michalsleezak@gmail.com>
2021-02-11 22:32:36 +0100

    Make AnswerFeedback an enum
    And make Jackson serialize it like a regular POJO.

--- src/main/java/mslezak2/web_quiz_engine/data/AnswerFeedback.java
+++ src/main/java/mslezak2/web_quiz_engine/data/AnswerFeedback.java
@@ -1,6 +1,8 @@
 package mslezak2.web_quiz_engine.data;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Entity;
 
 //TODO: To replace it with an enum class
@@ -9,35 +11,34 @@ import javax.persistence.Entity;
  * It consists of a boolean variable along with a String phrase that describe
  * the correctness of given answer.
  *  */
-public class AnswerFeedback {
-
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum AnswerFeedback {
+    
     //@JSONIgnore
-    public static final AnswerFeedback NEGATIVE_FEEDBACK
-            = new AnswerFeedback(false, "Wrong answer! Please, try again.");
+    NEGATIVE_FEEDBACK(false, "Wrong answer! Please, try again."),
     //@JSONIgnore
-    public static final AnswerFeedback POSITIVE_FEEDBACK
-            = new AnswerFeedback(true, "Congratulations, you're right!");
-
+    POSITIVE_FEEDBACK(true, "Congratulations, you're right!");
+    
     private boolean success;
     private String feedback;
-
-    public AnswerFeedback(boolean success, String feedback) {
+    
+    AnswerFeedback(boolean success, String feedback) {
         this.success = success;
         this.feedback = feedback;
     }
-
+    
     public boolean isSuccess() {
         return success;
     }
-
+    
     public void setSuccess(boolean success) {
         this.success = success;
     }
-
+    
     public String getFeedback() {
         return feedback;
     }
-
+    
     public void setFeedback(String feedback) {
         this.feedback = feedback;
     }
